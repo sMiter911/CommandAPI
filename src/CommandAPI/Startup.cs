f747ada552ddbc34cedeb5d367e83ea25bcb379c
Wandu <31390092+sMiter911@users.noreply.github.com>
2022-01-03 23:43:30 +0200

    Added Repository and Switched out Static Data

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace CommandAPI
 {
@@ -15,10 +17,20 @@ namespace CommandAPI
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CommandContext>(options => {
+                options.UseNpgsql(Configuration.GetConnectionString("PostgreSqlConnection"));
+            });
             services.AddControllers();
-            services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
+            services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
